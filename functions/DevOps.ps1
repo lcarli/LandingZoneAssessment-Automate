40707d4a1e33eabd340d4ca4e280fecedaa3a051
Lucas Humenhuk <lramoscostah@microsoft.com>
2024-09-09 14:30:52 -0400

    Base functions and main with export reports

---
+++ functions/DevOps.ps1
@@ -0,0 +1,31 @@
+<#
+.SYNOPSIS
+    Functions related to DevOps assessment.
+
+.DESCRIPTION
+    This script contains functions to evaluate the DevOps area of Azure Landing Zone.
+
+.LICENSE
+    MIT License
+
+.AUTHOR
+    lramoscostah@microsoft.com
+#>
+
+# Import shared modules
+Import-Module "$PSScriptRoot/../shared/Enums.ps1"
+Import-Module "$PSScriptRoot/../shared/ErrorHandling.ps1"
+
+function Invoke-DevOpsAssessment {
+    Write-Host "Evaluating the DevOps design area..."
+
+    $results = @()
+
+    # Call individual assessment functions
+
+
+    # Return the results
+
+
+   return $results
+}
---
+++ functions/Governance.ps1
@@ -0,0 +1,31 @@
+<#
+.SYNOPSIS
+    Functions related to Governance assessment.
+
+.DESCRIPTION
+    This script contains functions to evaluate the Governance area of Azure Landing Zone.
+
+.LICENSE
+    MIT License
+
+.AUTHOR
+    lramoscostah@microsoft.com
+#>
+
+# Import shared modules
+Import-Module "$PSScriptRoot/../shared/Enums.ps1"
+Import-Module "$PSScriptRoot/../shared/ErrorHandling.ps1"
+
+function Invoke-GovernanceAssessment {
+    Write-Host "Evaluating the Governance design area..."
+
+    $results = @()
+
+    # Call individual assessment functions
+
+
+    # Return the results
+
+
+   return $results
+}
